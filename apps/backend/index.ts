92951e2804bc0fa7ad7d20580cceb9dcd210bc83
divyanshusingh1101@gmail.com <divyanshusingh1101@gmail.com>
2025-09-30 19:27:14 +0000

    added backend

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,18 +2,86 @@ import express from "express";
 import cors from "cors";
 import { prismaClient } from "database/client";
 import { authMiddleware } from "./middleware";
+import jwt from "jsonwebtoken";
+import {SignupSchema} from "common/inputs";
+
 const app = express();
 app.use(cors());
 
-app.post("/signin", (req, res)=>{
-    res.send({
-        "msg":"hello this is signin endpoint"
+app.post("/signin", async(req, res)=>{
+    const {success, data} = SignupSchema.safeParse(req.body);
+    if(!success) {
+        res.status(403).json({
+            message:"Incorrect credentials 1"
+        })
+        return;
+    }
+    const email = data.email;
+    const password = data.password;
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email
+        }
+
     });
+
+    if (!user){
+        res.status(403).json({
+            message: "user not found"
+        })
+        return;
+    }
+
+    if(user.password !== password ){
+        res.status(403).json({
+            msg: "Incorrect password"
+        })
+        return;
+    }
+
+    const token = jwt.sign({
+        userId: user.id
+    }, process.env.JWT_SECRET!)
 })
 
-app.get("/calender", authMiddleware, (req, res) =>{
+app.get("/calender:courseId", authMiddleware, async(req, res) =>{
+    const courseId  = req.params.courseId;
+    const course = await prismaClient.course.findFirst({
+        where: {
+            id: courseId
+        }
+    })
+
+    const purchase = await prismaClient.purchases.findFirst({
+        where: {
+            userId: req.userId,
+            courseId: courseId
+        }
+    })
+
+    if(!purchase) {
+        res.status(411).json({
+            message: "you dont have access to this code."
+        })
+        return;
+    }
+
+    if (!course){
+        res.status(403).json({
+            message: "course with id not found"
+        })
+        return;
+    }
+
+    res.json({
+        id: course.id,
+        calender: course.calenderNotionId
+    })
+
 
 })
 
 
-app.listen(process.env || 3000);
+app.listen(3000);
+console.log("running on port 3000");
---
+++ packages/common/input.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const SignupSchema = z.object({
+    email: z.string(),
+    password: z.string(),
+});
+
